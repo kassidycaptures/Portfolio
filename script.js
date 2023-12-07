67acb4b3d42e63ec2cca4ff6ea5f59009178cc03
kassidycaptures <144375651+kassidycaptures@users.noreply.github.com>
2023-12-07 10:59:41 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("menu").addEventListener("click",function() {
+    document.getElementById("nav").classList.toggle("open")
+    document.getElementById("menu").classList.toggle("hide")
+    document.getElementById("menu-close").classList.toggle("open")
+})
+
+document.getElementById("menu-close").addEventListener("click",function() {
+document.getElementById("nav").classList.toggle("open")
+document.getElementById("menu").classList.toggle("hide")
+document.getElementById("menu-close").classList.toggle("open")
+})
